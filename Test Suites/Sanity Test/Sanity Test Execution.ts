3cd6eb761999eae28666f00efe1faa66138d017c
mvsl20167 <mvsl20167@192.168.1.103>
2021-01-27 15:06:48 +0530

    Initial commit

---
+++ Test Suites/Sanity Test/Sanity Test Execution.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Sanity Test Execution</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>b826634c-5b48-419a-bfa8-3ccc73f8cc1b</testSuiteGuid>
+   <testCaseLink>
+      <guid>1e73708f-8675-4391-a5f1-4bfd2e4365c7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/mvsl_CI-01</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1fecef81-1599-4bad-9ef8-5f84180615b8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/mvsl_CI-03</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>36f358f3-61a6-4500-8f47-5489dce8ae3b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/mvsl_CI-02</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
